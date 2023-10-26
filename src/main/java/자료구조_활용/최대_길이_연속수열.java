756d3852b113399b24b1c76ebd7b02cf10c9fbd4
bigstring.zer0 <bigstring.zero@gmail.com>
2023-10-26 22:41:52 +0900

    2022/10/26 practice of coding test.

---
+++ src/main/java/자료구조_활용/최대_길이_연속수열.java
@@ -0,0 +1,34 @@
+package 자료구조_활용;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class 최대_길이_연속수열 {
+    public int solution(int[] nums) {
+        int answer = Integer.MIN_VALUE;
+        Set<Integer> integerSet = new HashSet<>();
+        for (int num : nums) {
+            integerSet.add(num);
+        }
+        for (Integer integer : integerSet) {
+            if (integerSet.contains(integer - 1)){
+                continue;
+            }
+            int cnt = 0;
+            while (integerSet.contains(integer)) {
+                cnt++;
+                integer++;
+            }
+            answer = Math.max(answer, cnt);
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        최대_길이_연속수열 T = new 최대_길이_연속수열();
+        System.out.println(T.solution(new int[]{8, 1, 9, 3, 10, 2, 4, 0, 2, 3}));
+        System.out.println(T.solution(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0, 0}));
+        System.out.println(T.solution(new int[]{3, 3, 3, 3, 3, 3, 3, 3}));
+        System.out.println(T.solution(new int[]{-3, -1, -2, 0, 3, 3, 5, 6, 2, 2, 1, 1}));
+        System.out.println(T.solution(new int[]{-5, -3, -1, -4, 3, 3, 5, 6, 2, 2, 1, 1, 7}));
+    }
+}
