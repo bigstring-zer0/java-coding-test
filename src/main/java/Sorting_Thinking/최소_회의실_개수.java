fcc01f82da20b15e7f985dc1b11af3b51cb9d59f
bigstring.zer0 <bigstring.zero@gmail.com>
2023-11-20 14:41:25 +0900

    Changed class name.

--- src/main/java/Sorting_Thinking/최소_회의실_개수.java
+++ src/main/java/Sorting_Thinking/최소_회의실_개수.java
@@ -1,7 +1,7 @@
 package Sorting_Thinking;
 
 import java.util.*;
-class Solution {
+class 최소_회의실_개수 {
     public int solution(int[][] meetings){
         ArrayList<int[]> list = new ArrayList<>();
 
@@ -27,7 +27,7 @@ class Solution {
     }
 
     public static void main(String[] args){
-        Solution T = new Solution();
+        최소_회의실_개수 T = new 최소_회의실_개수();
         System.out.println(T.solution(new int[][]{{0, 10}, {20, 25}, {5, 15}, {2, 5}}));
         System.out.println(T.solution(new int[][]{{1, 30}, {2, 15}, {3, 10}, {4, 12}, {6, 10}}));
         System.out.println(T.solution(new int[][]{{3, 9}, {1, 10}, {5, 8}, {10, 15}, {9, 14}, {12, 14}, {15, 20}}));
